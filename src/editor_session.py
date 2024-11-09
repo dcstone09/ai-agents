b6dd99ef01ac72b02ce9afdbe6b31e7e821b8565
David Stone <dstone@grubmarket.com>
2024-11-09 16:45:21 +0000

    add anthropic

--- src/editor_session.py
+++ src/editor_session.py
@@ -1,3 +1,8 @@
+import anthropic
+import os
+
+
 class EditorSession:
     def __init__(self):
         self.logger = None
+        self.client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
